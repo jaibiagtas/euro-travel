3a784cd7834e2a0f4265ed866313aeda3fae0e68
Jai Biagtas <jai@MacBook-Pro.broadband>
2020-05-22 16:34:03 +0100

    update to locations/ index functions

---
+++ src/data/locations.js
@@ -0,0 +1,25 @@
+export const locations = [
+    {
+        placename: 'London, UK',
+        date: 'April 21, 2015' ,
+        location: {
+            lat: 51.5074,
+            lng: 0.1278
+        },
+        todo: [
+            'Where we start!'
+        ]
+    },
+    {
+        placename: 'Montpellier, France' ,
+        date: 'August, 2015' ,
+        location: {
+            lat: 43.6108,
+            lng: 3.8767
+        },
+        todo: [
+            'First trip!'
+        ]
+    }
+];
+
